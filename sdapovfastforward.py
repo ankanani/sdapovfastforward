ffb0d98aa5ceac3bd5214d56e5ea7c8a4f80f293
ankanani <ankanani@cisco.com>
2019-11-04 00:41:16 +0530

    minor edits

--- sdapovfastforward.py
+++ sdapovfastforward.py
@@ -9,7 +9,8 @@ import os
 import sys
 import subprocess
 
-print ("  === WELCOME TO THE SDA POV DEMO CONFIGURATION FAST-FORWARDING SCRIPT ===\n")
+print("")
+print("  === WELCOME TO THE SDA POV DEMO CONFIGURATION FAST-FORWARDING SCRIPT ===\n")
 
 # ask users to use python3 if they are on python2
 python_major_version = sys.version_info[0]
@@ -17,18 +18,18 @@ if python_major_version == 3:
     pass
 elif python_major_version == 2:
     print("IMPORTANT: THIS CODE IS NO LONGER SUPPORTED ON PYTHON V2. USE PYTHON V3 INSTEAD.\n")
-    raw_input("PRESS ENTER TO EXIT")
+    input("PRESS ENTER TO EXIT")
     os._exit(0)
 else:
     print("COULD NOT DETERMINE PYTHON VERSION. SO EXITING.\n")
-    raw_input("PRESS ENTER TO EXIT")
+    input("PRESS ENTER TO EXIT")
     os._exit(0)
 
 # checking for special python modules and installing them if not already present
 try:
     import git
 except:
-    print("==> INSTALLING THE REQUIRED PYTHON MODULE - gitpython")
+    print("==> INSTALLING THE REQUIRED PYTHON MODULE - gitpython\n")
     subprocess.call([sys.executable, "-m", "pip", "install", "gitpython"])
     import git
 try:
@@ -37,7 +38,7 @@ try:
     from urllib3.exceptions import InsecureRequestWarning
     requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 except:
-    print("==> INSTALLING THE REQUIRED PYTHON MODULE - requests")
+    print("==> INSTALLING THE REQUIRED PYTHON MODULE - requests\n")
     subprocess.call([sys.executable, "-m", "pip", "install", "requests"])
     import requests
     from requests.exceptions import HTTPError
@@ -52,15 +53,15 @@ SCRIPT_WORK_DIR_TEMP = os.path.join(".","sdapovfastforward","temp")
 # clone git repo if not exists already
 try:
     repo = git.Repo(".")
-    print("==> FOUND LOCAL GIT REPO")
+    print("==> FOUND LOCAL GIT REPO\n")
 except git.exc.InvalidGitRepositoryError:
-    print("==> LOCAL GIT REPO NOT FOUND. WILL FETCH ONE FROM THE INTERNET. YOU WILL SEE A NEW DIRECTORY CREATED AT THE END.")
+    print("==> LOCAL GIT REPO NOT FOUND. WILL FETCH ONE FROM THE INTERNET. YOU WILL SEE A NEW DIRECTORY CREATED AT THE END.\n")
     repo = git.Repo.clone_from(GIT_REPO_URL, SCRIPT_WORK_DIR)
     os.unlink(os.path.abspath(__file__))
     print ("==> JUST EXECUTE THIS SCRIPT AGAIN FROM sdapovfastforward DIRECTORY\n")
-    raw_input("PRESS ENTER TO EXIT")
+    input("PRESS ENTER TO EXIT")
     sys.exit(0)
 except Exception as e:
     print("==> GIT REPO READING EXCEPTION %s. SO EXITING." % str(e))
-    raw_input("PRESS ENTER TO EXIT")
+    input("PRESS ENTER TO EXIT")
     sys.exit(0)
